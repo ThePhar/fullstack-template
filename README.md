c01a525fc81b1dbd29ac7a07ddf1dc3f27484c7d
Zach Parks <zach@alliware.com>
2019-09-24 19:17:22 -0500

    fixed polyfill missing from build output for backend

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Fullstack MERN Template
+[![Build Status](https://travis-ci.org/ThePhar/fullstack-template.svg?branch=master)](https://travis-ci.org/ThePhar/fullstack-template)
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
-import * as mongoose from 'mongoose';
+import 'babel-polyfill';
+import mongoose from 'mongoose';
 import app from './app';
 
 const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
@@ -6,7 +7,9 @@ const port = process.env.PORT || 3000;
 
 (async () => {
   // Connect to the database.
-  await mongoose.connect(url, { useNewUrlParser: true });
+  if (process.env.MONGODB_PRESENT) {
+    await mongoose.connect(url, { useNewUrlParser: true });
+  }
 
   // Start listing on our express application.
   app.listen(port, () => console.log('Express listing on port 3000'));
